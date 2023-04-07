1d7606f602078ecd668855653295eca038c73782
SofiaKononovych <105751740+SofiaKononovych@users.noreply.github.com>
2023-04-07 13:50:31 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+class Student:
+    def __init__(self, name, surname, faculty, year_of_studying, group):
+        self.name = name
+        self.surname = surname
+        self.faculty = faculty
+        self.year_year_of_studying = year_of_studying
+        self.group = group
+
+
+class Teacher():
+    def __init__(self, name, surname, cathedra):
+        self.name = name
+        self.surname = surname
+        self.cathedra = cathedra
+
+
+
+class Faculty():
+
+    #create data of students?
+    #add stud/teacher
+    #add cathedra
+    #remove stud/teach
+    #remove cath
+    #
+
+
